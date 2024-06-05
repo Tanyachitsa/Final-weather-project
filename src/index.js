f3f8a83a4036c749ba39f68067d2107834b449fc
Tanyachitsa <165687341+Tanyachitsa@users.noreply.github.com>
2024-06-05 22:21:43 +0100

    added the search function with an API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+function updateWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function cityWeather(city) {
+  let apiKey = "24e8b867od685a3be0f6dd1ca9tbaf14";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function changeCity(event) {
   event.preventDefault();
-  let cityElement = document.querySelector("#city");
   let searchBox = document.querySelector(".text-box");
-  let theCity = searchBox.value;
-  let city = theCity[0].toUpperCase() + theCity.slice(1);
-  let searchEngine = (cityElement.innerHTML = city);
-  console.log(searchEngine);
+
+  cityWeather(searchBox.value);
 }
 
 let searchCity = document.querySelector(".search-glass");
 searchCity.addEventListener("click", changeCity);
+
+cityWeather("London");
